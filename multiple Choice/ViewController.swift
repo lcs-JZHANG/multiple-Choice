23df9cd5ec7b0afff0e7dd533721ce9e4a617b98
lcs-JZhang <XZhang@lcs.on.ca>
2019-12-09 15:29:56 -0500

    debug and finished

--- multiple Choice/ViewController.swift
+++ multiple Choice/ViewController.swift
@@ -9,8 +9,8 @@
 import UIKit
 
 class ViewController: UIViewController {
-
-// outlet
+    
+    // outlet
     
     @IBOutlet weak var numberOfQuestions: UITextField!
     
@@ -19,78 +19,81 @@ class ViewController: UIViewController {
     @IBOutlet weak var correctAnswers: UITextField!
     
     @IBOutlet weak var output: UITextView!
-
+    
     // methods
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
         
-    
+        
     }
-// action
+    // action
     @IBAction func checkAnswers(_ sender: Any) {
         
-    // Clear the output answer list
-           output.text = ""
-    //check the number of question
-    guard let numberOfQuestionsAsString = numberOfQuestions.text, numberOfQuestionsAsString.count>0 else {checkAnswers.text = "Please enter an integer value greater than 0"
-                    return
-                }
-    // make sure the number of questions are numbers
-    guard let numberOfQuestionsAsInt = Int(numberOfQuestionsAsString)else{
-                    checkAnswers.text = "Please enter an integer value greater than 0"
-                    return
-                }
-    // make sure two student answer input got the same number of answers
-    guard let studentAnswersAsString = studentAnswers.text,studentAnswersAsString.count == numberOfQuestionsAsInt
-                    else{
-                        checkAnswers.text = "Please be sure you input exactly \(numberOfQuestionsAsInt) student answers"
-                        return
-                }
-    //make sure two student answer input got the same number of answers
-guard let correctAnswersAsString = correctAnswers.text,correctAnswersAsString.count == numberOfQuestionsAsInt else{
-    checkAnswers.text = "Please be sure you input exactly \(numberOfQuestionsAsInt) answers for the answer key"
+        // Clear the output answer list
+        output.text = ""
+        
+        //check the number of question
+        guard let numberOfQuestionsAsString = numberOfQuestions.text, numberOfQuestionsAsString.count > 0 else {
+            output.text = "Please enter an integer value greater than 0"
+            return
+        }
+        
+        // make sure the number of questions are numbers
+        guard let numberOfQuestionsAsInt = Int(numberOfQuestionsAsString)else{
+            output.text = "Please enter an integer value greater than 0"
+            return
+        }
+        // make sure two student answer input got the same number of answers
+        guard let studentAnswersAsString = studentAnswers.text,studentAnswersAsString.count == numberOfQuestionsAsInt
+            else{
+                output.text = "Please be sure you input exactly \(numberOfQuestionsAsInt) student answers"
+                return
+        }
+        //make sure two student answer input got the same number of answers
+        guard let correctAnswersAsString = correctAnswers.text,correctAnswersAsString.count == numberOfQuestionsAsInt else{
+            output.text = "Please be sure you input exactly \(numberOfQuestionsAsInt) answers for the answer key"
+            return
+        }
+        // make sure the only answer are "A,B,C,D and E"
+        let allAnswer = "ABCDEabcde"
+        
+        for eachAnswer in studentAnswersAsString {
+            guard  allAnswer.contains(eachAnswer) else{
+                output.text = "Student answers contain invalid choices, please ensure only character A, B, C, D or E are used"
                 return
             }
-// make sure the only answer are "A,B,C,D and E"
-let allAnswer = "ABCDE"
-        
-for eachAnswer in studentAnswersAsString {
-guard  allAnswer.contains(eachAnswer) else{
-checkAnswers.text = "Student answers contain invalid choices, please ensure only character A, B, C, D or E are used"
-                    return
-                }
-            }
-for eachAnswer in correctAnswersAsString {
-guard allAnswer.contains(eachAnswer) else {
-                    checkAnswers.text = "Only answers A,B,C,D and E should be contained"
-                    return
-                }
-             }
- var index = -1
- var correctNumber = 0
-             
- for eachStudentAnswer in studentAnswersAsString{
-                 
- index += 1
- let theNthCorrectAnswers = correctAnswersAsString.index(correctAnswersAsString.startIndex, offsetBy: index)
-               
- if eachStudentAnswer == correctAnswersAsString[theNthCorrectAnswers] {
-                     correctNumber += 1
- }else{
-                     correctNumber += 0
-                 }
-             }
-     // show the final answer in the storyboard
-             checkAnswers.text = "The student answered \(correctNumber) question(s) correctly"
-            
-            
         }
+        for eachAnswer in correctAnswersAsString {
+            guard allAnswer.contains(eachAnswer) else {
+                output.text = "Only answers A,B,C,D and E should be contained"
+                return
+            }
+        }
+        var index = -1
+        var correctNumber = 0
+        
+        for eachStudentAnswer in studentAnswersAsString{
+            
+            index += 1
+            let theNthCorrectAnswers = correctAnswersAsString.index(correctAnswersAsString.startIndex, offsetBy: index)
+            
+            if eachStudentAnswer == correctAnswersAsString[theNthCorrectAnswers] {
+                correctNumber += 1
+            }else{
+                correctNumber += 0
+            }
+        }
+        // show the final answer in the storyboard
+        output.text = "The student answered \(correctNumber) question correctly"
         
         
     }
-       
-      
+    
+    
+}
+
+
 
 
 
